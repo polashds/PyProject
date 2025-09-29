0ac2ee7373ad731285d2165aa58a2d6b636bd47e
Polash <polashtechai@gmail.com>
2025-09-29 10:16:19 +0600

    intentional error raise added

--- pytest/test_square.py
+++ pytest/test_square.py
@@ -7,9 +7,16 @@ class TestSquare(unittest.TestCase):
         area = square.area()
         self.assertEqual(area, 100)
 
+
+    def test_length_with_wrong_type(self):
+        with self.assertRaises(TypeError):
+            square = Square("10")
+
 #Running tests without calling unittest.main()
 # if __name__ == '__main__':
 #     unittest.main(verbosity=2)
 #python -m unittest , -m stands for module
 # for more info
 # python -m unittest -v
+
+
