817c74961576e8612cc468d3f1a8fc2c9b996eda
Polash <polashtechai@gmail.com>
2025-09-29 10:30:51 +0600

    to make test pass another check added

--- pytest/square.py
+++ pytest/square.py
@@ -2,6 +2,9 @@ class Square:
     def __init__(self, length) -> None:
         if type(length) not in [int, float]:
             raise TypeError('length must be a number')
+        
+        if length <0:
+            raise ValueError('length must not be negative')
         self.length = length
 
     def area(self):
