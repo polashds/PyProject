21ff957a93c57640b741177839f03455803e9368
Polash <polashtechai@gmail.com>
2025-10-02 08:33:47 +0600

    method level test fixtures added

---
+++ unittest/test_fixtures.py
@@ -0,0 +1,21 @@
+#test fixtures is a step carried out before or after a test.
+# The setUpModule() function runs before all test methods in the test module.
+# The tearDownModule() function runs after all methods in the test module
+
+import unittest
+
+class setUpModule():
+    print('running setUpModule')
+
+class tearDownModule():
+    print('running tearDownModule')
+
+
+class TestMyModule(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEqual(5+5, 10)
+
+        def test_case_2(self):
+            self.assertEqual(1+1,2)
+
+# python -m unittest -v
